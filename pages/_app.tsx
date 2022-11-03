f0deae953a024eb313703ec12591dd747f7c9aee
Paweł Kromołowski <cater981pl@gmail.com>
2022-11-03 19:14:05 +0100

    vercel audience analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import Layout from '../components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
